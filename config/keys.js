76cf4c8b3b53825ec3b39acfa33bebd3d28d54b5
Matthew Hughes <matthew.l.hughes@kp.org>
2019-10-31 08:49:29 -0700

    initial

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://matt:matt123@cluster0-7w9rv.mongodb.net/test?retryWrites=true&w=majority"
+};
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(bodyParser.json());
+
+// DB config
+const db = require("./config/keys").mongoURI;
+
+// connect to Mongo
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected..."))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server started on port ${port}`));
